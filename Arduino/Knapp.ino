0297bd43fe0d328e9c47825a02af9756964abc47
Zebdawg <73936324+Zebdawg@users.noreply.github.com>
2020-12-09 12:24:45 +0100

    Add files via upload

---
+++ Arduino/Knapp.ino
@@ -0,0 +1,13 @@
+int knapp = 0;
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(13,INPUT);
+}
+void loop()
+{
+knapp = digitalRead(13);
+if (knapp == HIGH);
+  Serial.write("hei");
+  delay(1000);
+}
