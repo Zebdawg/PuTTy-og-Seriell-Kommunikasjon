01133bea28f3c3252869983edf9a1369a95f6dac
Zebdawg <73936324+Zebdawg@users.noreply.github.com>
2020-12-09 12:54:12 +0100

    Add files via upload

---
+++ Python/Seriell1.py
@@ -0,0 +1,20 @@
+import serial
+import time
+import datetime
+
+ser = serial.Serial(
+    'COM4', 9600, timeout=0, parity=serial.PARITY_NONE, rtscts=1
+)
+
+while True:
+    try:
+        s = str(ser.readline(100).decode())
+        if s != "":
+            f = open("Adgangslogg.txt", "a+")
+            f.write(s)
+
+    except:
+        print('ERROR')
+        break
+
+time.sleep(1)
